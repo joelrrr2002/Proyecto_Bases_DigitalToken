9a3c6c8f754516d474746c391b063de4975b1a0c
joelrrr2002 <81340144+joelrrr2002@users.noreply.github.com>
2022-08-03 23:25:58 -0600

    Create reg_libros_agregados.sql

---
+++ reg_libros_agregados.sql
@@ -0,0 +1,12 @@
+Create table dt.reg_libros_agregados(
+		id int Primary key
+		,num_serie int not null
+		,id_inventario int not null
+        ,fecha Nvarchar(50) not null
+        ,hora time not null
+        ,descripcion Nvarchar (50)
+
+);
+
+alter table reg_libros_agregados add constraint FK_NumSerie foreign key (num_serie) references Libros(num_serie);
+alter table reg_libros_agregados add constraint FK_Inventario foreign key (id_inventario) references Iventario(id_inventario);
